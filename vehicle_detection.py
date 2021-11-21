6378a5afb9d0ef0125f8914922ecf2d64e59ee75
bhoomeendra <bhooomeendra.pro@gmail.com>
2021-11-21 11:26:35 +0530

    'Done'

--- vehicle_detection.py
+++ vehicle_detection.py
@@ -2,12 +2,6 @@ import cv2
 
 
 class vehicle_detection(object):
-	def __init__(self, STREAM_URL, skip_steps=15):
-        self.cam = cv2.VideoCapture(STREAM_URL)
-        self.frame = None
-        self.skip_steps = skip_steps
-
-        
     def __init__(self, STREAM_URL, skip_steps=15):
         """
         > a frame-stream object
@@ -44,7 +38,7 @@ class vehicle_detection(object):
         This method applies all the pre-processing steps on the
         frame required for optimum performance.
         """
-		gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(gray, (21, 21), 0)
         return blurred
 
@@ -64,14 +58,41 @@ class vehicle_detection(object):
         This is the primary method to detect the motion between a old
         snapshot and current frame (separated by skip_steps number of frames)
         """
-
+        frameDeltaLast = cv2.absdiff(prev_frame, frame)
+        threshLast = cv2.threshold(frameDeltaLast, 25, 255, cv2.THRESH_BINARY)[1]
+        threshLast = cv2.dilate(threshLast, None, iterations=5)
+        contoursLast, hierL = cv2.findContours(threshLast.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for ct in contoursLast:
+            contour_area = cv2.contourArea(ct)
+            (x, y, w, h) = cv2.boundingRect(ct)
+            ct_area = contour_area / (h * w)
+            self.draw_bounding_Box(frame=self.frame,
+                                   contour=ct,
+                                   box_cord=(x, y, w, h))
+            if (ct_area < 0.01 or ct_area > 0.9) or (float(h) / w >= 1.8):
+                continue
+        cv2.imshow("Boxed frame", self.frame)
 
 
     def runner(self):
         """
-        Primary runner function to perform vehicle detection
+        Primary runner function to perform vehicle detection on Video Stream
         """
-
+        count = 0
+        self.get_frame()
+        prev_frame_BGR = self.frame
+        prev_frame_ppr = self.pre_process_frame()
+        while True:
+            while count != self.skip_steps:
+                self.get_frame()
+                count += 1
+            frame_ppr = self.pre_process_frame()
+            count = 0
+            self.detect_motion(prev_frame_ppr, frame_ppr)
+            prev_frame_ppr = frame_ppr.copy()
+            if cv2.waitKey(100) == ord('q'):
+                print("Runner stopped")
+                break
 
 
 if __name__ == "__main__":
